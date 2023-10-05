8e66ce848c432da4f7bd56feea4b30199a87c6cf
Aritra Bose <43315603+anbose@users.noreply.github.com>
2023-10-05 19:01:35 +0200

    Update declarations.h

--- declarations.h
+++ declarations.h
@@ -1,1 +1,2 @@
 void send_boundary(const int N, const int count, const int bnum, const double *localrho, double *leftboundary, double *rightboundary);
+void send_boundary_from_global(const int N, const int bnum, const int *firstindex, const double *globalrho, double *leftboundary, double *rightboundary);
